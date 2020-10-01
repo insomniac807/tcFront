45d782843e90a34f8af7701b5a0f04b636e620de
insomniac807 <philipbutler25@gmail.com>
2020-10-01 10:36:12 +0100

    server compiling and running. serving index.html on root(/)

--- server/app/tripcam-server.js
+++ server/app/tripcam-server.js
@@ -3,30 +3,32 @@ const readFileSync = require('fs').readFileSync,
       async = require('async'),
       express = require('express'),
       websocket = require('websocket'),
+      bodyParser = require('body-parser'),
+      cors = require('cors'),
       { Room, Rooms } = require('./rooms');
 
 
 class TripcamServer {
   constructor(config) {
     this.config = config;
-
     this.rooms = new Rooms(this.config.rooms);
-
     this.app = this.setupExpress();
     this.https = this.setupHttps(this.config, this.app);
-
     this.configureWebRoutes(this.config, this.app);
-
     this.wss = this.setupWss(this.config, this.https);
-  }
+  }//constructor
+
 
   setupExpress() {
     var app = express();
+    app.use(bodyParser.json());
+    app.use(cors());
     // app.set('views', this.config.express.viewsPath);
     // app.set('view engine', 'pug');
 
     return app;
-  }
+  }//setupExpress
+
 
   setupHttps(config, app) {
     // Create and configure HTTPS server
@@ -34,21 +36,25 @@ class TripcamServer {
       key: readFileSync(config.https.keyPath),
       cert: readFileSync(config.https.certPath)
     }, app);
-  }
+  }//setupHttps
+
 
   configureWebRoutes(config, app) {
     // Configure home routes
-    // app.get('/', (req, res) => {
-    //   res.render('index');
-    // });
-
+    app.get('/', (req, res) => {
+        var path = require('path');
+        console.log(path.resolve('../public/index.html'));
+        console.log(__dirname);
+        res.sendFile(path.resolve('../public/index.html'));
+     });
     // app.get('/room/:roomid', ((req, res) => {
     //     res.render('room', { roomid: req.params.roomid, serverAddress: this.config.wss.serverAddress });
     // }).bind(this));
 
     // Configure Static files route
-    // app.use(express.static(this.config.express.staticPath));
-  }
+    app.use(express.static(this.config.express.staticPath));
+  }//configureWebRoutes
+
 
   setupWss(config, https) {
     // Create and configure websocket server
@@ -56,46 +62,40 @@ class TripcamServer {
       httpServer: https,
       autoAcceptConnections: false
     });
-
     wss.on('request', (req => {
       if(!config.wss.allowedOrigins.includes(req.origin)) {
         console.log("rejecting wss: not allowed origin");
         req.reject();
         return;
       }
-
       if(!req.resource.startsWith('/room/')) {
         console.log("rejecting wss: wrong url");
         req.reject();
         return;
       }
-
       console.log("accepting wss");
-
       var cn = req.accept('room-protocol', req.origin);
       var roomid = req.resource.slice(6);
       this.rooms.createRoom(roomid);
-
       var clientid = "client" + Math.random().toString(36).substr(2, 9);
-
       this.rooms.newClient(roomid, clientid, cn);
     }).bind(this));
-
     return wss;
-  }
+  }//setupWss
+
 
   start() {
     this.https.listen(this.config.https.listenPort);
     console.log("Now listening for HTTPS connections on port " + this.config.https.listenPort);
-  }
+  }//start
+
 
   stop(done) {
     async.parallel({
-      https: (function(cb) {
+      https: (function(cb) { //#### MAY NEED TO EDIT THIS LINE (HTTPS:)
         console.log("stopping https");
         this.https.close(cb);
       }).bind(this),
-
       wss: (function(cb) {
         setImmediate((function() {
           console.log("stopping wss");
@@ -108,8 +108,9 @@ class TripcamServer {
       this.rooms.destroyAllRooms();
       console.log("shutdown complete");
       done();
-    }).bind(this));
-  }
+        }
+    ).bind(this));
+  }//stop
 }
 
 module.exports = TripcamServer;
